6d5e77dea4b832aca8e17c582b3ca6019968d470
SleepyPandaBear <mf8974@student.uni-lj.si>
2022-07-19 14:28:55 +0200

    added command line argument parsing

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,9 +3,14 @@ package main
 import (
     "net/http"
     "log"
+    "flag"
 )
 
 func main() {
+    // Parse value from command line (value must be dereferenced when used)
+    ip := flag.String("ip", ":8080", "Ip address and port of the application")
+    flag.Parse()
+
     mux := http.NewServeMux()
     mux.HandleFunc("/", home)
     mux.HandleFunc("/snippet", showSnippet)
@@ -17,8 +22,7 @@ func main() {
     mux.Handle("/static/", http.StripPrefix("/static", fs))
 
     // run server from console as: w:\snippetbox>go run .\cmd\web\
-    log.Println("Starting server on port 8080")
-    err := http.ListenAndServe(":8080", mux)
+    log.Printf("Starting server on port %s \n", *ip)
+    err := http.ListenAndServe(*ip, mux)
     log.Fatal(err)
-
 }
