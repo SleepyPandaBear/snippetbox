20862d268259f5bb60738192d378be2202bd9cc2
SleepyPandaBear <mf8974@student.uni-lj.si>
2022-07-19 14:47:45 +0200

    created custom loggers - infoLog & errorLog

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,12 +4,19 @@ import (
     "net/http"
     "log"
     "flag"
+    "os"
 )
 
 func main() {
     // Parse value from command line (value must be dereferenced when used)
     ip := flag.String("ip", ":8080", "Ip address and port of the application")
     flag.Parse()
+
+    // Our custom loggers, one for infos and one for errors.
+    // We can also define here to which file we should output the logs (eg.
+    // instead of os.Stdout).
+    infoLog := log.New(os.Stdout, "INFO:\t", log.Ldate | log.Ltime)
+    errorLog := log.New(os.Stderr, "ERROR:\t", log.Ldate | log.Ltime | log.Lshortfile)
 
     mux := http.NewServeMux()
     mux.HandleFunc("/", home)
@@ -21,8 +28,16 @@ func main() {
     // Add endpoint for the static files
     mux.Handle("/static/", http.StripPrefix("/static", fs))
 
+    // We need to create our own `Server` struct, so we can use our own error
+    // logger.
+    srv := &http.Server{
+        Addr: *ip,
+        ErrorLog: errorLog,
+        Handler: mux,
+    }
+
     // run server from console as: w:\snippetbox>go run .\cmd\web\
-    log.Printf("Starting server on port %s \n", *ip)
-    err := http.ListenAndServe(*ip, mux)
-    log.Fatal(err)
+    infoLog.Printf("Starting server on port %s \n", *ip)
+    err := srv.ListenAndServe(*ip, mux)
+    errorLog.Fatal(err)
 }
