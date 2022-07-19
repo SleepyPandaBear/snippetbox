e24794258e655190abf10b3dda761788c1f09447
SleepyPandaBear <mf8974@student.uni-lj.si>
2022-07-19 10:07:47 +0200

    added static file server

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -11,6 +11,11 @@ func main() {
     mux.HandleFunc("/snippet", showSnippet)
     mux.HandleFunc("/snippet/create", createSnippet)
 
+    // Serve static files under ./ui/static
+    fs := http.FileServer(http.Dir("./ui/static"))
+    // Add endpoint for the static files
+    mux.Handle("/static/", http.StripPrefix("/static", fs))
+
     // run server from console as: w:\snippetbox>go run .\cmd\web\
     log.Println("Starting server on port 8080")
     err := http.ListenAndServe(":8080", mux)
