f5432a4fe913677ca02b431e3369a527b781443b
SleepyPandaBear <mf8974@student.uni-lj.si>
2022-07-19 16:18:41 +0200

    created helpers for dealing with errors

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -5,13 +5,12 @@ import (
     "fmt"
     "strconv"
     "html/template"
-    "log"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
     // If we are not on the index page, show not found page.
     if r.URL.Path != "/" {
-        http.NotFound(w, r)
+        app.notFound(w)
         return
     }
 
@@ -24,8 +23,7 @@ func home(w http.ResponseWriter, r *http.Request) {
     // Try to parse our html template file
     ts, err := template.ParseFiles(files...)
     if err != nil {
-        log.Println(err.Error())
-        http.Error(w, "Internal server error", 500)
+        app.serverError(w, err)
         return
     }
 
@@ -33,26 +31,25 @@ func home(w http.ResponseWriter, r *http.Request) {
     // the template, here we don't pass anyting so its nil.
     err = ts.Execute(w, nil)
     if err != nil {
-        log.Println(err.Error())
-        http.Error(w, "Internal server error", 500)
+        app.serverError(w, err)
     }
 }
 
-func showSnippet(w http.ResponseWriter, r *http.Request) {
+func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 
     // r.URL.Quury().Get("id") returns value of id from url - /snippet?id=123
     // returns 123
     // Validate that id is int greater than 0
     id, err := strconv.Atoi(r.URL.Query().Get("id"))
     if err != nil || id < 1 {
-        http.NotFound(w, r)
+        app.notFound(w)
         return
     }
 
     fmt.Fprintf(w, "Display snippet with id: %d", id)
 }
 
-func createSnippet(w http.ResponseWriter, r *http.Request) {
+func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
     // Check if we are using POST method on this endpoint.
     if r.Method != "POST" {
         w.Header().Set("Allow", "POST")
@@ -60,7 +57,7 @@ func createSnippet(w http.ResponseWriter, r *http.Request) {
         // This code is replaced with http.Error(...)
         //w.WriteHeader(405)
         //w.Write([]byte("Method not allowed"))
-        http.Error(w, "Method not allowed", 405)
+        app.clientError(w, http.StatusMethodNotAllowed)
         return
     }
 
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,6 +7,11 @@ import (
     "os"
 )
 
+type application struct {
+    infoLog *log.Logger
+    errorLog *log.Logger
+}
+
 func main() {
     // Parse value from command line (value must be dereferenced when used)
     ip := flag.String("ip", ":8080", "Ip address and port of the application")
@@ -18,10 +23,20 @@ func main() {
     infoLog := log.New(os.Stdout, "INFO:\t", log.Ldate | log.Ltime)
     errorLog := log.New(os.Stderr, "ERROR:\t", log.Ldate | log.Ltime | log.Lshortfile)
 
+    // Our application struct so we can use our custom loggers from handlers.go
+    // file. If our handlers are scattered across multiple files, we can use
+    // function closures (first function accepts app and it returns another
+    // function with ResponseWriter and Request) and pass application struct as
+    // argument.
+    app := &application {
+        infoLog: infoLog,
+        errorLog: errorLog,
+    }
+
     mux := http.NewServeMux()
-    mux.HandleFunc("/", home)
-    mux.HandleFunc("/snippet", showSnippet)
-    mux.HandleFunc("/snippet/create", createSnippet)
+    mux.HandleFunc("/", app.home)
+    mux.HandleFunc("/snippet", app.showSnippet)
+    mux.HandleFunc("/snippet/create", app.createSnippet)
 
     // Serve static files under ./ui/static
     fs := http.FileServer(http.Dir("./ui/static"))
@@ -30,7 +45,7 @@ func main() {
 
     // We need to create our own `Server` struct, so we can use our own error
     // logger.
-    srv := &http.Server{
+    srv := &http.Server {
         Addr: *ip,
         ErrorLog: errorLog,
         Handler: mux,
@@ -38,6 +53,6 @@ func main() {
 
     // run server from console as: w:\snippetbox>go run .\cmd\web\
     infoLog.Printf("Starting server on port %s \n", *ip)
-    err := srv.ListenAndServe(*ip, mux)
+    err := srv.ListenAndServe()
     errorLog.Fatal(err)
 }
