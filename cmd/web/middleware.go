6d3471dd5f767a8cea08f1e911b77ef3dc2388e6
SleepyPandaBear <mf8974@student.uni-lj.si>
2022-07-21 06:13:50 +0200

    Comment how to recover from panic from a go routine

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -21,6 +21,22 @@ func (app *application) logRequest(next http.Handler) http.Handler {
     })
 }
 
+// This only recover from panics in this go routine. If we spawn another go
+// routines we need to check if they panic in different way (those go routines
+// failiures can bring down the server). This different way is shown bellow:
+// func myHandler(w http.ResponseWriter, r *http.Request) {
+//     ...
+//     // Spin up a new goroutine to do some background processing.
+//     go func() {
+//         defer func() {
+//             if err := recover(); err != nil {
+//             log.Println(fmt.Errorf("%s\n%s", err, debug.Stack()))
+//         }
+//         }()
+//         doSomeBackgroundProcessing()
+//     }()
+//     w.Write([]byte("OK"))
+// }
 func (app *application) recoverPanic(next http.Handler) http.Handler {
     return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
         defer func() {
