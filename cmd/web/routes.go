6006d9ec3084a251da0b1908ef02d8b74a264e69
SleepyPandaBear <mf8974@student.uni-lj.si>
2022-07-22 08:51:03 +0200

    Unathorized users now cannot acess /snippet/create and /logout endpoints

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -8,15 +8,15 @@ import (
 func (app *application) routes() http.Handler {
     mux := pat.New()
     mux.Get("/", app.session.Enable(http.HandlerFunc(app.home)))
-    mux.Get("/snippet/create", app.session.Enable(http.HandlerFunc(app.createSnippetForm)))
-    mux.Post("/snippet/create", app.session.Enable(http.HandlerFunc(app.createSnippet)))
+    mux.Get("/snippet/create", app.session.Enable(app.requireAuthenticatedUser(http.HandlerFunc(app.createSnippetForm))))
+    mux.Post("/snippet/create", app.session.Enable(app.requireAuthenticatedUser(http.HandlerFunc(app.createSnippet))))
     mux.Get("/snippet/:id", app.session.Enable(http.HandlerFunc(app.showSnippet)))
 
     mux.Get("/user/signup", app.session.Enable(http.HandlerFunc(app.signupUserForm)))
     mux.Post("/user/signup", app.session.Enable(http.HandlerFunc(app.signupUser)))
     mux.Get("/user/login", app.session.Enable(http.HandlerFunc(app.loginUserForm)))
     mux.Post("/user/login", app.session.Enable(http.HandlerFunc(app.loginUser)))
-    mux.Post("/user/logout", app.session.Enable(http.HandlerFunc(app.logoutUser)))
+    mux.Post("/user/logout", app.session.Enable(app.requireAuthenticatedUser(http.HandlerFunc(app.logoutUser))))
 
     // Serve static files under ./ui/static
     fs := http.FileServer(http.Dir("./ui/static"))
