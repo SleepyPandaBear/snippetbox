7975b6a98ac198694c227c1e8e0072c8bf41af1f
SleepyPandaBear <mf8974@student.uni-lj.si>
2022-07-22 14:19:00 +0200

    testing handler ping

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -8,6 +8,10 @@ import (
     "spbear/snippetbox/pkg/forms"    
 )
 
+func ping(w http.ResponseWriter, r *http.Request) {
+    w.Write([]byte("OK"))
+}
+
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
     s, err := app.snippets.Latest()
     if err != nil {
